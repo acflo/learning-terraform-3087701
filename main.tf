f89949bde3cf8d43547bc0e68426f3f16caa14e3
acflo <98936918+acflo@users.noreply.github.com>
2023-04-02 12:35:08 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
+  subnet_id     = "subnet-013f88f40e623624b"
 
   tags = {
     Name = "HelloWorld"
