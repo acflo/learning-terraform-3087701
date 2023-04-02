0753f3ef74dc76e41f3baa5e6e34b53052362d2c
acflo <98936918+acflo@users.noreply.github.com>
2023-04-02 12:43:01 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,19 @@
 data "aws_ami" "app_ami" {
   most_recent = true
-
   filter {
     name   = "name"
     values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
   owners = ["979382823631"] # Bitnami
 }
-
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
   subnet_id     = "subnet-013f88f40e623624b"
-
   tags = {
     Name = "HelloWorld"
   }
